dcfc7956345e2c4bdae0aa8acfa5e7a3ac2f3076
richard dinh <dick.dinh@gmail.com>
2019-04-01 07:30:59 -0400

    uppdate build script

--- example/script.js
+++ example/script.js
@@ -3,14 +3,15 @@ var geojson2mvt = require('../src');
 
 var options = {
   layers: {
-    layer0: JSON.parse(fs.readFileSync('bus_routes.geojson', "utf8")),
-    layer1: JSON.parse(fs.readFileSync('stops.geojson', "utf8"))
+   // layer0: JSON.parse(fs.readFileSync('2017-zip.geojson', "utf8")),
+    //layer0: JSON.parse(fs.readFileSync('2017-cong.geojson', "utf8"))
+    layer0: JSON.parse(fs.readFileSync('2015-tract.geojson', "utf8"))
   },
-  rootDir: 'tiles',
-  bbox : [40.426042,-74.599228,40.884448,-73.409958], //[south,west,north,east]
+  rootDir: 'tiles/tract',
+  bbox : [18.91619,-171.791110603,71.3577635769,-66.96466], //[south,west,north,east]
   zoom : {
-    min : 8,
-    max : 18,
+    min : 12,
+    max : 14,
   }
 };
 // build the static tile pyramid
